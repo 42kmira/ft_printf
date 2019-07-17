0c57d913648a92f6911c3310b1a652d543a3f321
Kevin Mira <kmira@e1z2r5p16.42.us.org>
2019-07-16 17:59:14 -0700

    o conversion

---
+++ srcs/conversions/o_conversions.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   o_conversions.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmira <kmira@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/16 17:58:44 by kmira             #+#    #+#             */
+/*   Updated: 2019/07/16 17:59:03 by kmira            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+t_string	x_handler(t_format *format, unsigned long long value)
+{
+	t_string	result;
+	int			arg_size;
+
+	arg_size = get_arguement_size(format->specifier[0], format->length);
+	result.output = ft_ntoa(value, 0, arg_size, NUM_SET, 8);
+	result.length = ft_strlen(result.output);
+	result.free = TRUE;
+	return (result);
+}
